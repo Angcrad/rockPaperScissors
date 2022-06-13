e9b0b20ee9846556eb0bfee1595424245d4ff58d
Angcrad <angcrad@gmail.com>
2022-06-13 18:37:37 -0500

    changed to button play

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-game();
-function game()
-{
-    let playerSelection;
-    let computerSelection;
-    let win=0;
-    let lose=0;
-    let tie=0;
-    let result=0;
-    for(let i=0;i<5;i++)
+let win=0;
+let lose=0;
+let tie=0;
+const rockBtn=document.getElementById("r");
+const paperBtn=document.getElementById("p");
+const scissorBtn=document.getElementById("s");
+rockBtn.addEventListener("click",function()
     {
-        playerSelection = prompt("rock/paper/scissors?: ").toLowerCase();
+    game("r");
+    }
+    );
+paperBtn.addEventListener("click",function()
+    {
+    game("p");
+    }
+    );
+scissorBtn.addEventListener("click",function()
+    {
+    game("s");
+    }
+    );
+function game(playerSelection)
+{
+    let computerSelection;
+    let result=0;
+    //for(let i=0;i<5;i++)
+    //{
         switch(playerSelection)
         {
             case "r":
@@ -26,24 +41,30 @@ function game()
         switch(playRound(playerSelection,computerSelection))
         {
             case 1:
-                console.log("your "+playerSelection+" won against computer's "+computerSelection)
-                win++;
+                document.getElementById("status").innerText="your "+playerSelection+" won against computer's "+computerSelection;
+                document.getElementById("win").innerText="wins: "+(++win);
                 break;
             case -1:
-                console.log("your "+playerSelection+" lost against computer's "+computerSelection)
-                lose++;
+                document.getElementById("status").innerText="your "+playerSelection+" lost against computer's "+computerSelection;
+                document.getElementById("lose").innerText="loses: "+(++lose);
                 break;
             case 0:
-                console.log("your "+playerSelection+" tied with computer's "+computerSelection)
-                tie++;
+                document.getElementById("status").innerText="your "+playerSelection+" tied with computer's "+computerSelection;
+                document.getElementById("tie").innerText="ties: "+(++tie);
                 break;
             default:
                 console.log("invalid input: "+playerSelection);
-                i--;
+                //i--;
+                break;
         }
-        console.log((4-i)+" plays left");
-    }
-    result=win-lose;
+        if(win===5||lose===5)
+        {
+            anounceWinner(win,lose);
+        }
+        
+        //console.log((4-i)+" plays left");
+    //}
+    /*result=win-lose;
     if(result===0)
     {
         console.log("it's a tie");
@@ -55,7 +76,28 @@ function game()
     else if(result<0)
     {
         console.log("you lost "+win+"-"+lose);
+    }*/
+}
+function anounceWinner(lwin,llose)
+{
+    let outString="you ";
+    if(lwin>llose)
+    {
+        outString+="won";
     }
+    else
+    {
+        outString+="lost";
+    }
+    outString+=(" "+win+"-"+lose+" with "+tie+" ties");
+    win=0;
+    lose=0;
+    tie=0;
+    alert(outString);
+    document.getElementById("status").innerText="";
+    document.getElementById("win").innerText="wins: 0";
+    document.getElementById("lose").innerText="loses: 0";
+    document.getElementById("tie").innerText="ties: 0";
 }
 function computerPlay()
 {
